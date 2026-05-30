827867d548c8ee758eb8ebb9a22259866176f4b8
ARPITSONI6266 <arpitsoni62003@gmail.com>
2026-05-30 21:14:09 +0000

    adding revise

---
+++ 01_basic/04_revise.js
@@ -0,0 +1,18 @@
+// primiitive
+
+// 7 type : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+//js is a dynamic language
+
+//reference (Non primitive)
+
+//Array, Objects, Functions
+const heros = ["shatiman", "naagraj", "doga"]
+let myObj = {
+    name: "arpit",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
